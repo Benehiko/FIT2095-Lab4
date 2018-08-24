425c0d75ae8cd9ee21c9de48a0357cf5e4f566d6
Alano Terblanche <alanopi.314@gmail.com>
2018-08-24 09:13:24 +0200

    Final

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var indexRouter = require('./routes/index.js');
 var bodyParser = require('body-parser');
-var path=require('path');
+var path = require('path');
 var app = express();
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.use(express.static('views'));
-app.use(express.static('public/images'));
-app.use(express.static('bower_components'));
-
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static('bower_components'));
+app.use('/bootstrap', express.static(path.join(__dirname, 'bower_components/bootstrap/')));
+app.use('/jquery', express.static(path.join(__dirname, 'bower_components/jquery/')));
+app.use('/css', express.static(path.join(__dirname, 'public/stylesheets/')));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
